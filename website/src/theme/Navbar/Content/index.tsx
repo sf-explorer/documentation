0349b8abfdcf95e166b8536a7a82487782278c74
nicolas-despres <ndespres@gmail.com>
2022-10-25 09:49:47 +0200

    chore: bump dependencies

--- website/src/theme/Navbar/Content/index.tsx
+++ website/src/theme/Navbar/Content/index.tsx
@@ -5,21 +5,95 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React, {type ReactNode} from 'react';
-import type {Props as NavbarItemConfig} from '@theme/NavbarItem';
-import NavbarItem from '@theme/NavbarItem';
-import NavbarColorModeToggle from '@theme/Navbar/ColorModeToggle';
-import SearchBar from '@theme/SearchBar';
-import {
-  splitNavbarItems,
-  useNavbarMobileSidebar,
-  useThemeConfig,
-} from '@docusaurus/theme-common';
-import NavbarMobileSidebarToggle from '@theme/Navbar/MobileSidebar/Toggle';
-import NavbarLogo from '@theme/Navbar/Logo';
-import styles from './styles.module.css';
+ import React, {type ReactNode} from 'react';
+ import {useThemeConfig} from '@docusaurus/theme-common';
+ import {
+   splitNavbarItems,
+   useNavbarMobileSidebar,
+ } from '@docusaurus/theme-common/internal';
+ import NavbarItem, {type Props as NavbarItemConfig} from '@theme/NavbarItem';
+ import NavbarColorModeToggle from '@theme/Navbar/ColorModeToggle';
+ import SearchBar from '@theme/SearchBar';
+ import NavbarMobileSidebarToggle from '@theme/Navbar/MobileSidebar/Toggle';
+ import NavbarLogo from '@theme/Navbar/Logo';
+ import NavbarSearch from '@theme/Navbar/Search';
+ 
 import Avatar from '@site/src/components/Avatar'
+ import styles from './styles.module.css';
+ 
+ function useNavbarItems() {
+   // TODO temporary casting until ThemeConfig type is improved
+   return useThemeConfig().navbar.items as NavbarItemConfig[];
+ }
+ 
+ function NavbarItems({items}: {items: NavbarItemConfig[]}): JSX.Element {
+   return (
+     <>
+       {items.map((item, i) => (
+         <NavbarItem {...item} key={i} />
+       ))}
+     </>
+   );
+ }
+ 
+ function NavbarContentLayout({
+   left,
+   right,
+ }: {
+   left: ReactNode;
+   right: ReactNode;
+ }) {
+   return (
+     <div className="navbar__inner">
+       <div className="navbar__items">{left}</div>
+       <div className="navbar__items navbar__items--right">{right}</div>
+     </div>
+   );
+ }
+ 
+ export default function NavbarContent(): JSX.Element {
+   const mobileSidebar = useNavbarMobileSidebar();
+ 
+   const items = useNavbarItems();
+   const [leftItems, rightItems] = splitNavbarItems(items);
+ 
+   const searchBarItem = items.find((item) => item.type === 'search');
+ 
+   return (
+     <NavbarContentLayout
+       left={
+         // TODO stop hardcoding items?
+         <>
+           {!mobileSidebar.disabled && <NavbarMobileSidebarToggle />}
+           <NavbarLogo />
+           <NavbarItems items={leftItems} />
+           
+         </>
+       }
+       right={
+         // TODO stop hardcoding items?
+         // Ask the user to add the respective navbar items => more flexible
+         <>
+           <NavbarItems items={rightItems} />
+           <NavbarColorModeToggle className={styles.colorModeToggle} />
+           <Avatar />
+           {!searchBarItem && (
+             <NavbarSearch>
+               <SearchBar />
+             </NavbarSearch>
+           )}
+         </>
+       }
+     />
+   );
+ }
+ 
 
+
+
+
+/*
+/////////
 function useNavbarItems() {
   // TODO temporary casting until ThemeConfig type is improved
   return useThemeConfig().navbar.items as NavbarItemConfig[];
@@ -81,3 +155,4 @@ export default function NavbarContent(): JSX.Element {
     />
   );
 }
+*/
